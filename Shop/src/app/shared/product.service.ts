1ad9d421bf458b73cb108aebf20bbbd0346a3a20
Andrey-Yur <andrei.iurchenko@easternstudent.ca>
2020-07-28 00:35:39 -0300

    main page, upload items on main page from FB

--- Shop/src/app/shared/product.service.ts
+++ Shop/src/app/shared/product.service.ts
@@ -20,4 +20,15 @@ export class ProductService {
         }
       }))
   }
+  getAll() {
+    return this.http.get(`${environment.fbDbUrl}/products.json`)
+      .pipe(map(res => {
+        return Object.keys(res)
+          .map(key => ({
+            ...res[key],
+            id: key,
+            data: new Date(res[key].data)
+          }))
+      }))
+  }
 }
